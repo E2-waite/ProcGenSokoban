1dc55e8a287ce50eca46e886fb3d5f7a16a6f185
E2-Waite <E2-Waite>
2020-01-10 16:04:04 +0000

    Prevent rooms with noncontinuous floors
    Once room is generated check if all floor tiles are interconnected, if they are not room layout is discarded.

---
+++ Sokoban Prototype/Assets/Scripts/CheckSurrounding.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckSurrounding : MonoBehaviour
+{
+    public void CheckAdjascent (List<GameObject> floor_tiles, GameObject grid)
+    {
+        GenerateGrid grid_script = grid.GetComponent<GenerateGrid>();
+        foreach (GameObject obj in floor_tiles)
+        {
+            float distance = Vector3.Distance(transform.position, obj.transform.position);
+
+            if (distance == 1 && !grid_script.checked_floors.Contains(obj))
+            {
+                grid_script.AddToChecked(obj);
+                CheckSurrounding next_check = obj.GetComponent<CheckSurrounding>();
+                next_check.CheckAdjascent(floor_tiles, grid);
+            }
+        }
+    }
+}
--- Sokoban Prototype/Assets/Scripts/GenerateGrid.cs
+++ Sokoban Prototype/Assets/Scripts/GenerateGrid.cs
@@ -14,7 +14,8 @@ public class GenerateGrid : MonoBehaviour
     private GameObject[,] object_grid;
     private int[,] grid;
     private bool selected = false;
-
+    private List<GameObject> floor_tiles = new List<GameObject>();
+    [HideInInspector]  public List<GameObject> checked_floors = new List<GameObject>();
     public void Start()
     {
         StartCoroutine(SetupGrid());
@@ -23,6 +24,12 @@ public class GenerateGrid : MonoBehaviour
     private IEnumerator SetupGrid()
     {
         if (selected || num_instances >= max_instances) yield break;
+
+        if (object_grid != null)
+        {
+            floor_tiles.Clear();
+            checked_floors.Clear();
+        }
 
         num_instances++;
  
@@ -81,7 +88,18 @@ public class GenerateGrid : MonoBehaviour
     }
 
     private void CreateGrid()
-    {      
+    {
+        if (object_grid != null)
+        {
+            for (int i = 0; i < object_grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < object_grid.GetLength(1); j++)
+                {
+                    Destroy(object_grid[i, j]);
+                }
+            }
+        }
+
         object_grid = new GameObject[size_x * 3, size_y * 3];
         for (int x = 0; x < size_x * 3; x++)
         {
@@ -89,7 +107,7 @@ public class GenerateGrid : MonoBehaviour
             {
                 if (grid[x, y] == 1)
                 {
-                    object_grid[x,y] = Instantiate(floor_prefab, new Vector3(x, 0, y), Quaternion.identity);
+                    floor_tiles.Add(object_grid[x,y] = Instantiate(floor_prefab, new Vector3(x, 0, y), Quaternion.identity));
                 }
                 else if (grid[x, y] == 2)
                 {
@@ -97,5 +115,32 @@ public class GenerateGrid : MonoBehaviour
                 }
             }
         }
+
+        StartCoroutine(StartFloorChecks());
+    }
+
+
+    IEnumerator StartFloorChecks()
+    {
+        CheckSurrounding floor_check = floor_tiles[0].GetComponent<CheckSurrounding>();
+        floor_check.CheckAdjascent(floor_tiles, this.gameObject);
+        yield return new WaitForSeconds(0.1f);
+        if (checked_floors.Count < floor_tiles.Count)
+        {
+            Debug.Log("Floor Not Continuous - Discarding Layout...");
+            selected = false;
+            num_instances = 0;
+            StartCoroutine(SetupGrid());
+        }
+        else
+        {
+            Debug.Log("Continuous Floor");
+            // Continue to next layout check
+        }
+    }
+
+    public void AddToChecked(GameObject tile)
+    {
+        checked_floors.Add(tile);
     }
 }
