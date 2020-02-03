14c59c366fdf3de25bc4f5580cae0dd94684a752
E2-Waite <E2-Waite>
2020-02-03 17:27:25 +0000

    Fixed hallway section positioning
    Fixed the issue causing hallway sections being generated undesirable distance away from previous section. Now results in continuous hallway.
    Next the system needs to prevent sections from overlapping with each other, and deal with templates with multiple exits (create closed loop).

--- Sokoban Prototype/Assets/HallwayGenerator.cs
+++ Sokoban Prototype/Assets/HallwayGenerator.cs
@@ -11,7 +11,7 @@ public class HallwayGenerator : MonoBehaviour
     Vector2 start_dir = new Vector2(0, 1);
     public int num_segments = 1;
     List<GameObject[,]> obj_grids = new List<GameObject[,]>();
-    bool lined_up;
+    bool lined_up, flipped;
     void Start()
     {
         templates = GetComponent<Templates>();
@@ -19,55 +19,44 @@ public class HallwayGenerator : MonoBehaviour
         StartCoroutine(LoadTemplate(Random.Range(0, 4), num_segments, start_dir, start_pos));
     }
 
-    IEnumerator LoadTemplate(int temp_num, int steps_left, Vector2 dir, Vector2 last_pos)
+    IEnumerator LoadTemplate(int temp_num, int steps_left, Vector2 dir, Vector2 pos)
     {
         float x_offset = 0, y_offset = 0;
-        Vector2 entrance_pos = new Vector2(0,0);
-        Vector2 exit_pos = new Vector2(0, 0);
         template = templates.GetHall(temp_num);
         lined_up = false;
-        // Find entrance and exit positions
-        for (int y = 0; y < 5; y++)
-        {
-            for (int x = 0; x < 5; x++)
-            {
-                if (template[x, y] == (int)TILE.enterance)
-                {
-                    entrance_pos = new Vector2(x, y);
-                }
-                if (template[x, y] == (int)TILE.exit)
-                {
-                    exit_pos = new Vector2(x, y);
-                }
-            }
-        }
+        flipped = false;
 
-        StartCoroutine(RotateTemplate(dir));
+        // Flip template 50% of the time
+        bool flip_template = (Random.value > 0.5f);
+        if (flip_template) FlipTemplate();
+        else flipped = true;
+        while (!flipped) yield return null;
+
         //Wait until section is lined up
+        StartCoroutine(RotateTemplate(dir));
         while (!lined_up) yield return null;
 
         // Calculate section offset
         if (dir == Vector2.right)
         {
-            x_offset = (int)last_pos.x + 5;
-            y_offset = 0;
+            x_offset = (int)pos.x + 5;
+            y_offset = (int)pos.y;
         }
         if (dir == Vector2.left)
         {
-            x_offset = (int)last_pos.x - 5;
-            y_offset = 0;
+            x_offset = (int)pos.x - 5;
+            y_offset = (int)pos.y;
         }
         if (dir == Vector2.up)
         {
-            x_offset = 0;
-            y_offset = (int)last_pos.y + 5;
+            x_offset = (int)pos.x;
+            y_offset = (int)pos.y + 5;
         }
         if (dir == Vector2.down)
         {
-            x_offset = 0;
-            y_offset = (int)last_pos.y - 5;
+            x_offset = (int)pos.x;
+            y_offset = (int)pos.y - 5;
         }
-        last_pos = new Vector2(last_pos.x + x_offset, last_pos.y + y_offset);
 
         // Generate hallway section
         GameObject[,] grid = new GameObject[5, 5];
@@ -93,8 +82,8 @@ public class HallwayGenerator : MonoBehaviour
                     grid[x, y] = Instantiate(floor_prefab, new Vector3(x + x_offset, 0, y + y_offset), Quaternion.identity);
                     if (x == 0) dir = Vector2.left;
                     if (x == 4) dir = Vector2.right;
-                    if (y == 0) dir = Vector2.up;
-                    if (y == 4) dir = Vector2.down;
+                    if (y == 0) dir = Vector2.down;
+                    if (y == 4) dir = Vector2.up; 
                     grid[x, y].name = "EXIT";
                 }
             }
@@ -102,24 +91,37 @@ public class HallwayGenerator : MonoBehaviour
         }
         obj_grids.Add(grid);
 
+        pos = new Vector2(x_offset, y_offset);
         steps_left--;
-        if (steps_left > 0) StartCoroutine(LoadTemplate(Random.Range(0, 4), steps_left, dir, last_pos));
-        else
+        if (steps_left > 0) StartCoroutine(LoadTemplate(Random.Range(0, 4), steps_left, dir, pos));
+        else Debug.Log("HALLWAY FINISHED");
+    }
+
+    private void FlipTemplate()
+    {
+        // Flip along x axis
+        int[,] flipped_template = new int[5, 5];
+        for (int y = 0; y < 5; y++)
         {
-            Debug.Log("FINISHED");
+            int new_x = 4;
+            for (int x = 0; x < 5; x++)
+            {
+                flipped_template[x, y] = template[new_x, y];
+                new_x--;
+            }
         }
+        template = flipped_template;
+        flipped = true;
     }
 
     IEnumerator RotateTemplate(Vector2 dir)
     {
-        Debug.Log("STARTED ROTATION, DIR: " + dir.x.ToString() + dir.y.ToString());
         int[,] temp_template = template;
         Vector2 desired_start = new Vector2(0,0);
         if (dir == Vector2.right) desired_start = new Vector2(0, 2);
         if (dir == Vector2.left) desired_start = new Vector2(4, 2);
         if (dir == Vector2.up) desired_start = new Vector2(2, 0);
         if (dir == Vector2.down) desired_start = new Vector2(2, 4);
-        Debug.Log("DESIRED START: " + desired_start.x.ToString() + desired_start.y.ToString());
 
         // Check if current orientation's entrace matches previous section's exit
         for (int y = 0; y < 5; y++)
@@ -147,7 +149,6 @@ public class HallwayGenerator : MonoBehaviour
                 {
                     if (temp_template[x, y] == (int)TILE.enterance)
                     {
-                        Debug.Log("X: " + x.ToString() + " Y: " + y.ToString());
                         if (x == desired_start.x && y == desired_start.y)
                         {
                             template = temp_template;
@@ -159,13 +160,12 @@ public class HallwayGenerator : MonoBehaviour
                 }
             }
         }
-        Debug.Log("FAILED ROTATION");
     }
 
     static int[,] RotateTemplate(int[,] template)
     {
+        // Rotate template 90 degrees clockwise
         int[,] rot_template = new int[5, 5];
-
         for (int i = 0; i < 5; ++i)
         {
             for (int j = 0; j < 5; ++j)
@@ -173,7 +173,6 @@ public class HallwayGenerator : MonoBehaviour
                 rot_template[i, j] = template[5 - j - 1, i];
             }
         }
-
         return rot_template;
     }
 }
